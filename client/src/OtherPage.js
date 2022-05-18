572da9fbc1562ba346741131155e6ab82a64a3b0
mhonzirek <marek.honzirek@xitee.com>
2022-05-18 09:06:53 +0200

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,10 @@
+import { Link} from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            In some other page!
+            <Link to="/">Go back home</Link>
+        </div>
+    );
+}
